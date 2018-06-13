77ee4fda94e0505176805411a35f59ddfb7fef8a
Christoph Welcz <christoph@welcz.de>
2018-06-13 18:37:07 +0200

    refactored a bit

--- code/src/main/java/RomanNumbersConverter.java
+++ code/src/main/java/RomanNumbersConverter.java
@@ -1,5 +1,5 @@
-public class RomanNumbersConverter {
-    public String toRoman(int arabic) {
+class RomanNumbersConverter {
+    String toRoman(int arabic) {
         return null;
     }
 }
--- code/src/test/java/RomanNumbersConverterTest.java
+++ code/src/test/java/RomanNumbersConverterTest.java
@@ -2,11 +2,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RomanNumbersConverterTest {
+class RomanNumbersConverterTest {
     @Test
     void convertToEmpty() {
         var converter = new RomanNumbersConverter();
-        String result = converter.toRoman(0);
-        assertThat(result).isEmpty();
+        assertThat(converter.toRoman(0)).isEmpty();
     }
 }
